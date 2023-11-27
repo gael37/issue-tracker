af39507f243a8825e9df0a0c4aea701bd32a9918
gael37 <gx.duchesne@gmail.com>
2023-11-27 02:43:18 +0000

    min height

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -35,7 +35,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const issueCount = await prisma.issue.count({ where });
 
   return (
-    <section className='landing-section-list'>
+    <section className='landing-section'>
       <div className='max-w-6xl  mx-auto p-5 pt-10'>
         <Flex direction="column" gap="3">
           <IssueActions />
